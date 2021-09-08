4e85511432b08fdfbca9266f1a8acab1393b0618
Jose Sanchez <hi@josesanchez.dev>
2021-09-08 07:55:38 -0500

    made small changes to loading from json

--- src/main/java/dev/teamdoom/DefaultScene.java
+++ src/main/java/dev/teamdoom/DefaultScene.java
@@ -5,19 +5,32 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 public class DefaultScene implements IScene {
+	public enum POINT_TYPE {
+		PLAYER_REP, PLAYER_INT, PLAYER_FIT
+	};
 	private UUID id;
+	private POINT_TYPE point_type;
 	private String question;
 	private ArrayList<String> choices;
 	private ArrayList<Integer> choicePoints;
 	private ArrayList<String> responses;
 
 	public DefaultScene(UUID id, String question, ArrayList<String> choices, ArrayList<Integer> choicePoints,
-			ArrayList<String> responses) {
+			ArrayList<String> responses, POINT_TYPE pointTypeFromUser) {
 		this.setId(id);
+		this.setPointType(pointTypeFromUser);
 		this.setQuestion(question);
 		this.setChoices(choices);
 		this.setChoicePoints(choicePoints);
 		this.setResponses(responses);
+	}
+
+	public POINT_TYPE getPointType() {
+		return point_type;
+	}
+
+	public void setPointType(POINT_TYPE point_type) {
+		this.point_type = point_type;
 	}
 
 	public ArrayList<String> getResponses() {
@@ -68,6 +81,7 @@ public class DefaultScene implements IScene {
 
 	@Override
 	public void printChoices() {
+<<<<<<< Updated upstream
 		// TODO Auto-generated method stub
 
 	}
@@ -76,6 +90,22 @@ public class DefaultScene implements IScene {
 	public int getUserChoiceSelection(Console c) {
 		// TODO Auto-generated method stub
 		return 0;
+=======
+		for(int i = 1; i <= choices.size(); i++){
+			System.out.println(i + ") " + choices.get(i - 1));
+		}
+	}
+
+	//gets the selections
+	public int getUserChoiceSelection() {
+		System.out.println("You can enter the choice number, from 1 to " + (responses.size()));
+		try {
+		int userChoiceSelection = Integer.parseInt(System.console().readLine()); //maybe later we can also enter the choice by name
+		return userChoiceSelection - 1;
+
+	} catch(Exception e) {
+		}
+>>>>>>> Stashed changes
 	}
 
 	@Override
